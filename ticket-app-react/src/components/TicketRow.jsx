3febd0ab81acb17f19b4d906c2bbe5c50b7a1663
Emmanuel Ihemedu <147402567+DeraTheOz@users.noreply.github.com>
2025-10-29 13:52:45 +0100

    fix: change default ticket description text

--- ticket-app-react/src/components/TicketRow.jsx
+++ ticket-app-react/src/components/TicketRow.jsx
@@ -5,7 +5,7 @@ function TicketRow({ ticket }) {
         {ticket.title}
       </td>
       <td className="table-ec19de06-ade5-48db-9432-08f80a721c73-column-240 h-[72px] px-4 py-2 w-[400px] text-[#4c739a] text-sm font-normal leading-normal">
-        {ticket.description || "Not specified"}
+        {ticket.description || "No description"}
       </td>
       <td className="h-[72px] px-4 py-2 w-60 text-sm font-normal leading-normal">
         <button
